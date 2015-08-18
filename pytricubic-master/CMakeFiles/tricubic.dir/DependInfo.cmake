8777cc20740fde95d3dcdeb40233680826c29bdf
totocaca <pueduponey@gmail.com>
2015-08-18 14:17:58 -0400

    0.1.0

---
+++ pytricubic-master/CMakeFiles/tricubic.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/p0054421/MEGA/calcul/LCS_tractor/pytricubic-master/tricubic.cpp" "/home/p0054421/MEGA/calcul/LCS_tractor/pytricubic-master/CMakeFiles/tricubic.dir/tricubic.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/include/python2.7"
+  "/usr/include/x86_64-linux-gnu/python2.7"
+  "/usr/local/include/eigen3"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ pytricubic-master/CMakeFiles/tricubic.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/tricubic.dir/tricubic.cpp.o"
+  "tricubic.pdb"
+  "tricubic.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/tricubic.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
